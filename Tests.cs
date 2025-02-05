0f4871eb3535f3258d665c17764890026362ef3d
KorpenM <magnuskorp@hotmail.com>
2025-02-05 14:51:10 +0100

    Adding test for encryption

--- Tests.cs
+++ Tests.cs
@@ -5,6 +5,23 @@ namespace cicdApi
 {
     public class Tests
     {
+        private readonly EncryptionService _encryptionService;
 
+        public Tests()
+        {
+            // Skapa en instans av EncryptionService med en nyckel
+            _encryptionService = new EncryptionService(3); // Caesar Cipher med nyckel 3
+        }
+
+        [Fact]
+        public void Encrypt_ShouldReturnEncryptedText()
+        {
+            var input = "abc";
+            var expected = "def";
+
+            var result = _encryptionService.Encrypt(input);
+
+            Assert.Equal(expected, result);
+        }
     }
 }
