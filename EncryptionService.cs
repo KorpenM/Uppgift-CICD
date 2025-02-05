b5b228073b693350c8ddf53966d9e4135a367bb0
KorpenM <magnuskorp@hotmail.com>
2025-02-05 11:11:34 +0100

    Added method for encrypt by using Caesar Cipher

--- EncryptionService.cs
+++ EncryptionService.cs
@@ -1,6 +1,36 @@
-﻿namespace cicdApi
+﻿using System.Text;
+
+
+namespace cicdApi
 {
     public class EncryptionService
     {
+        private int _key;
+
+        public EncryptionService(int key)
+        {
+            _key = key;
+        }
+
+        // Krypterar texten genom att använda Caesar Cipher
+        public string Encrypt(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            var result = new StringBuilder();
+            foreach (char c in input)
+            {
+                if (char.IsLetter(c))
+                {
+                    var start = char.IsUpper(c) ? 'A' : 'a';
+                    result.Append((char)((((c - start + _key) + 26) % 26) + start));
+                }
+                else
+                {
+                    result.Append(c);
+                }
+            }
+            return result.ToString();
+        }
     }
 }
