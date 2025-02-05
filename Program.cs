e878644f62ebb045ba4da3256f8d741b90a6e731
KorpenM <magnuskorp@hotmail.com>
2025-02-05 11:24:16 +0100

    Adding code to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
+using EncryptionAPI;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
