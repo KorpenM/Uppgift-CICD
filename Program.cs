e581ee81a2f0e9f405a814e7cc85633c851ebbf4
KorpenM <magnuskorp@hotmail.com>
2025-02-05 11:27:01 +0100

    adding MapGet to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using cicdApi;
 using EncryptionAPI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -5,3 +6,15 @@ using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+// Skapar en instans av EncryptionService med en nyckel
+int key = 3;
+var encryptionService = new EncryptionService(key);
+
+app.MapGet("/", () => "Tjena kompis!");
+
+app.MapGet("/encrypt", (string input) => encryptionService.Encrypt(input));
+
+app.MapGet("/decrypt", (string input) => encryptionService.Decrypt(input));
+
+app.Run();
