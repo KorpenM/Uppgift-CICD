6c4a3518c7c51bbed5180875ac3c9641eada471b
KorpenM <magnuskorp@hotmail.com>
2025-02-07 08:57:30 +0100

    Added PW generator

--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
 using cicdApi;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Skapar en instans av EncryptionService med en nyckel
 int key = 3;
 var encryptionService = new EncryptionService(key);
 
 app.MapGet("/", () => "Tjena kompis!");
 
 app.MapGet("/encrypt", (string input) => encryptionService.Encrypt(input));
-
 app.MapGet("/decrypt", (string input) => encryptionService.Decrypt(input));
+
+// Genererar och krypterar ett slumpmässigt lösenord
+app.MapGet("/generate-password", () =>
+{
+    var password = encryptionService.GenerateRandomPassword();
+    var encryptedPassword = encryptionService.Encrypt(password);
+    return new { Original = password, Encrypted = encryptedPassword };
+});
 
 app.Run();
