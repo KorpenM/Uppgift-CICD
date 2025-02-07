d8ae5a980d9ae2a32b4325b97b495317cea71427
KorpenM <magnuskorp@hotmail.com>
2025-02-07 09:02:22 +0100

    Made some updates to base code for functionality

--- Program.cs
+++ Program.cs
@@ -8,17 +8,16 @@ var app = builder.Build();
 int key = 3;
 var encryptionService = new EncryptionService(key);
 
-app.MapGet("/", () => "Tjena kompis!");
+app.MapGet("/", () => "Tjena kompis! Generera ett password med /generate-password");
 
 app.MapGet("/encrypt", (string input) => encryptionService.Encrypt(input));
 app.MapGet("/decrypt", (string input) => encryptionService.Decrypt(input));
 
-// Genererar och krypterar ett slumpmässigt lösenord
+// Genererar och krypterar ett slumpmässigt lösenord men returnerar bara den krypterade texten
 app.MapGet("/generate-password", () =>
 {
     var password = encryptionService.GenerateRandomPassword();
-    var encryptedPassword = encryptionService.Encrypt(password);
-    return new { Original = password, Encrypted = encryptedPassword };
+    return encryptionService.Encrypt(password);
 });
 
 app.Run();
